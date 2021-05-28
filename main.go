668f2481aec26da9ca5077872670c85af1b4a737
sakura-rip <56917703+sakura-rip@users.noreply.github.com>
2021-05-28 17:10:33 +0900

    add: cmds validator

--- main.go
+++ main.go
@@ -10,5 +10,25 @@ func main() {
 		log.Fatal().Msg("Target directory is required")
 	}
 	cmds := GetCommandLists(os.Args[len(os.Args)-1])
+	Validate(cmds)
 	GenerateCommandHandler(cmds)
+}
+
+func Validate(cmds []Command) {
+	var ids []string
+	for _, cmd := range cmds {
+		if isContain(ids, cmd.ID) {
+			log.Fatal().Msg("cant use same id: " + cmd.ID)
+		}
+		ids = append(ids, cmd.ID)
+	}
+}
+
+func isContain(base []string, target string) bool {
+	for _, val := range base {
+		if target == val {
+			return true
+		}
+	}
+	return false
 }
