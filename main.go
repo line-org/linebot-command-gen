05374e304dde075d8b41e606a1d42c71180fa09c
sakura-rip <56917703+sakura-rip@users.noreply.github.com>
2021-05-28 20:28:48 +0900

    add: ディレクトリに対応

--- main.go
+++ main.go
@@ -2,14 +2,32 @@ package main
 
 import (
 	"github.com/phuslu/log"
+	"io/fs"
 	"os"
+	"path/filepath"
+	"strings"
 )
 
 func main() {
 	if len(os.Args) == 1 {
 		log.Fatal().Msg("Target directory is required")
 	}
-	cmds := GetCommandLists(os.Args[len(os.Args)-1])
+	path := os.Args[len(os.Args)-1]
+	fInfo, err := os.Stat(path)
+	if err != nil {
+		log.Fatal().Err(err).Msg("failed check path")
+	}
+	var cmds []Command
+	if fInfo.IsDir() {
+		filepath.Walk(path, func(path string, info fs.FileInfo, err error) error {
+			if strings.HasSuffix(path, "yaml") {
+				cmds = append(cmds, GetCommandLists(path)...)
+			}
+			return nil
+		})
+	} else {
+		cmds = GetCommandLists(path)
+	}
 	Validate(cmds)
 	GenerateCommandHandler(cmds)
 }
