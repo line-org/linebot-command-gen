ac58f03b8a784543c54f7f219943e777cadb883f
sakura-rip <56917703+sakura-rip@users.noreply.github.com>
2021-05-28 16:57:06 +0900

    change: gen output dir

--- gen.go
+++ gen.go
@@ -34,7 +34,8 @@ func GenerateCommandHandler(cmds []Command) {
 	if err != nil {
 		log.Fatal().Err(err).Msg("failed to load tempalte file")
 	}
-	path := "gene!!"
+	os.MkdirAll("gen", os.ModePerm)
+	path := "gen/commands.go"
 	file, err := os.OpenFile(path, os.O_CREATE|os.O_WRONLY, 0664)
 	if err != nil {
 		log.Fatal().Err(err).Msg("failed to open target file")
